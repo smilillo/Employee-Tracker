0562ff4f4b9527761c8f8d0a7a7c2cd5bd98dd90
smilillo <sofiemilillo@gmail.com>
2023-04-13 11:55:43 -0400

    installed inquirer, console.table, and mysql2

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// GIVEN a command-line application that accepts user input
-
-// WHEN I start the application
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 
 const db = mysql.createConnection(
     {
@@ -37,7 +35,7 @@ const menuOptions = [
     }
 ];
 
-function start() {
+function menu() {
     inquirer.prompt(menuOptions)
         .then(answer => {
             if (answer === "View All Departments"){
@@ -58,33 +56,47 @@ function start() {
         });
 }
 // WHEN I choose to view all departments
-
 // THEN I am presented with a formatted table showing department names and department ids
+function viewDepartments() {
+
+};
 
 // WHEN I choose to view all roles
-
 // THEN I am presented with the job title, role id, the department that role belongs to, 
 // and the salary for that role
+function viewRoles() {
+
+};
 
 // WHEN I choose to view all employees
-
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, 
 // last names, job titles, departments, salaries, and managers that the employees report to
+function viewEmployees() {
+
+};
 
 // WHEN I choose to add a department
-
 // THEN I am prompted to enter the name of the department and that department is added to the database
+function addDepartment() {
+
+};
 
 // WHEN I choose to add a role
-
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+function addRole() {
+
+};
 
 // WHEN I choose to add an employee
-
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, 
 // and that employee is added to the database
+function addEmployee() {
+
+};
 
 // WHEN I choose to update an employee role
-
 // THEN I am prompted to select an employee to update and their new role and this information is 
 // updated in the database
+function updateRole() {
+
+};
